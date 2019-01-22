abd4d478f13507dfa75c2006fe2c296a1766c15f
Graeme Congreve <junkmailgmc@gmail.com>
2019-01-22 14:59:55 +0000

    added file class, created database

---
+++ src/main/java/com/codeclan/filesandfolders/filesandfolders/models/Folder.java
@@ -0,0 +1,56 @@
+package com.codeclan.filesandfolders.filesandfolders.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "folders")
+public class Folder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "folder", fetch = FetchType.LAZY)
+    private List<File> files;
+
+    public Folder(String title) {
+        this.title = title;
+        this.files = new ArrayList<>();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<File> getFiles() {
+        return files;
+    }
+
+    public void setFiles(List<File> files) {
+        this.files = files;
+    }
+
+    public void addFile(File file){
+        this.files.add(file);
+    }
+}
